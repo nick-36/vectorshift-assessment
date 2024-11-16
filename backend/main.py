6a84394fe7309cbdc03e24b37ba1267f56214f0c
nick-36 <makwananikhil36@gmail.com>
2024-11-16 21:43:48 +0530

    refactored

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from typing import List, Dict
+from typing import List
 import networkx as nx
 from fastapi.middleware.cors import CORSMiddleware
 
